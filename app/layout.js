29d964e724e5a4f1de7810cbe4fcbcb92e02d319
Sanket0798 <sanketrathod1398@gmail.com>
2025-01-18 18:03:49 +0530

    Minor Changes.

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,3 @@
-// import { Poppins } from "next/font/google";
-// import "./styles/globals.css";
-// import HomePage from "./page";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
-
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   weight: ["300", "400", "500", "600", "700", "800"],
-//   variable: "--font-poppins",
-// });
-
-// const pockota = Poppins({
-//   subsets: ["latin"],
-//   weight: ["400"],
-//   variable: "--font-pockota",
-// });
-
-// export const metadata = {
-//   title: "Krishna Digital Media",
-//   description: "Krishna Digital Media",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={`${poppins.variable} antialiased font-sans font-manrope font-pockota`}>
-//         <HomePage>{children}</HomePage>
-//         <SpeedInsights />
-//       </body>
-//     </html>
-//   );
-// }
-
 import { Poppins } from "next/font/google";
 import "./styles/globals.css";
 import HomePage from "./page";
