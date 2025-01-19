fb52b0667c5888391757b1a5512912b6bb64e947
Sanket0798 <sanketrathod1398@gmail.com>
2025-01-19 21:40:52 +0530

    Footer has been completed with responsive part.

--- app/component/layout/Footer.jsx
+++ app/component/layout/Footer.jsx
@@ -6,11 +6,14 @@ import { socialLinks, contactDetails, navLinks } from "../../data/socialLinks";
 
 const Footer = () => {
   return (
-    <footer className="bg-footer text-white pt-[70px]">
+    <footer className="bg-footer text-white pt-[20px] md:pt-[70px] ">
       <div className="container mx-auto text-center">
-        <div className="flex-center-between-row">
+        <div className="flex flex-col md:flex-row md:justify-between">
           <div className="flex-start-between-col space-y-3">
-            <Link href="/">
+            <Link
+              href="/"
+              className="w-full flex-center-center md:flex-start-between-col"
+            >
               <Image
                 src="/assets/images/HomePage/Footer/NameLogo.png"
                 width={100}
@@ -18,13 +21,13 @@ const Footer = () => {
                 alt="Krishna Media Logo"
               />
             </Link>
-            <p className="text-sm leading-6 w-[311px] text-start">
+            <p className="text-base md:text-sm leading-6 w-full md:w-[311px] text-start">
               Krishna Media - The leading digital agency based in India, working
               with top-tier clients, from start-ups to enterprises.
             </p>
           </div>
 
-          <div className="text-white flex-start-between-col space-y-5">
+          <div className="text-white flex-start-between-col space-y-5 mt-7 md:mt-0">
             {contactDetails.map(({ icon, text }, index) => (
               <div key={index} className="flex-center-center space-x-5">
                 <span>{icon}</span>
@@ -49,9 +52,9 @@ const Footer = () => {
           </div>
         </div>
 
-        <hr className="bg-[#404040] mt-14 mb-7 w-full h-[1px]" />
+        <hr className="bg-[#404040] mt-4 md:mt-14 mb-5 w-full h-[1px]" />
 
-        <div className="flex-center-between flex-wrap pb-7">
+        <div className="flex flex-col md:flex-row md:justify-between pb-7">
           <div className="flex space-x-8">
             {navLinks.map((link, index) => (
               <a key={index} href="#" className="hover:opacity-80">
@@ -60,7 +63,7 @@ const Footer = () => {
             ))}
           </div>
 
-          <div className="mt-4 md:mt-0">
+          <div className="mt-4 md:mt-0 text-center md:text-right">
             &copy; {new Date().getFullYear()} Krishna Digital Media. All rights
             reserved.
           </div>
