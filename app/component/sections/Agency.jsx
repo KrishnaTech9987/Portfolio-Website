dd80b31c728802aec270abfe3210b712fd365ca2
krishnatech9987 <krishnadigitaltech@gmail.com>
2025-02-14 11:37:06 +0530

    Modification in Marquee component.

--- app/component/sections/Agency.jsx
+++ app/component/sections/Agency.jsx
@@ -1,11 +1,11 @@
-  "use client";
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Button } from "../common/Button";
 
 const Agency = () => {
   return (
-    <section className="py-16 px-4">
+    <section className="py-8 md:py-16 px-4">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
--- app/component/ui/marquee.js
+++ app/component/ui/marquee.js
@@ -25,20 +25,20 @@ const Marquee = () => {
     }, []);
 
     return (
-        <div className="relative overflow-hidden whitespace-nowrap w-full bg-gray-800 text-black py-4">
-            <div className="absolute top-0 left-0 h-full w-20 bg-gradient-to-r from-gray-800 to-transparent"></div>
-            <div className="flex flex-center-between-row items-center space-x-12" ref={marqueeRef}>
+        <div className="relative overflow-hidden whitespace-nowrap w-full bg-gray-800 text-black py-2 sm:py-3 md:py-4">
+            <div className="absolute top-0 left-0 h-full w-12 sm:w-16 md:w-20 bg-gradient-to-r from-gray-800 to-transparent z-10"></div>
+            <div className="flex flex-center-between-row items-center space-x-4 xs:space-x-6 sm:space-x-8 md:space-x-10 lg:space-x-12" ref={marqueeRef}>
                 {[...Array(2)].flatMap(() => MarketingServices).map((service, index) => (
-                    <div key={index} className="flex items-center space-x-12">
-                        <span className="text-sm font-medium dark:text-white dark:text-white/40">
+                    <div key={index} className="flex items-center space-x-4 xs:space-x-6 sm:space-x-8 md:space-x-10 lg:space-x-12">
+                        <span className="text-xs xs:text-sm sm:text-sm md:text-sm lg:text-sm font-medium dark:text-white dark:text-white/40">
                             {service.name}
                         </span>
-                        <Star />
+                        <Star className="w-3 h-3 xs:w-4 xs:h-4 sm:w-5 sm:h-5 md:w-6 md:h-6" />
                     </div>
                 ))}
             </div>
-            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
-            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/5 sm:w-1/4 bg-gradient-to-r from-background z-10"></div>
+            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/5 sm:w-1/4 bg-gradient-to-l from-background z-10"></div>
         </div>
     );
 };
